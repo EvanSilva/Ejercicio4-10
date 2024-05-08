9bd0e8a3b4fd0428f9fb62cc04a0859231d224df
a20evansg <a20evansg@192.168.56.1>
2024-05-08 11:20:37 +0200

    PrimerCommit NetBeans Ej 4.10 parte D branch Dev

---
+++ src/main/java/EjercicioGitEvan/Restador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioGitEvan;
+
+/**
+ *
+ * @author a20evansg
+ */
+public class Restador {
+    public static float restar (float a, float b ){
+    return a - b;
+}}
