d0964bed001a14071b7536d2ad754140dd6a072b
a20evansg <a20evansg@192.168.56.1>
2024-05-08 11:09:33 +0200

    PrimerCommit NetBeans Ej 4.10

---
+++ src/main/java/EjercicioGitEvan/Sumador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EjercicioGitEvan;
+
+/**
+ *
+ * @author a20evansg
+ */
+
+    public class Sumador {
+        public static float sumar (float a, float b ) {
+    return a + b;
+    }
+
+    }
